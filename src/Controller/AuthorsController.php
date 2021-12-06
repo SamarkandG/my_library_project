36e6ab851a35c0342d8ec7d9191d1dd0cf96e950
SamarkandG <samarkand.guinot@gmail.com>
2021-12-06 17:04:55 +0100

    Afficher les auteurs !

--- src/Controller/AuthorsController.php
+++ src/Controller/AuthorsController.php
@@ -8,11 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AuthorsController extends AbstractController
 {
-
+// Ici je créer une route "un accès" pour mon URL, pour accéder à la page des auteurs
     /**
      * @Route ("/authors", name="authors")
      */
 
+    // Ici je créer une fonction qui va me permettre d'accéder aux données voulues lorsque j'utiliserais le mot "authors" dans une mage twig
     public function authors(AuthorsRepository $authorsRepository)
     {
         // Ici je me sers de AUTHORREPOSITORY pour afficher l'ensemble des livres stockés en base de données sur ma page HTML
